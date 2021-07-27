7ccb49493a04c9f61287bfd702d374b12d2f8cb1
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-27 19:35:45 +0700

    Создает модули навигации и сортировки

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createProfileTemplate } from './views/profile.js';
+import { createNavigationTemplate } from './views/navigation.js';
+import { createSortTemplate } from './views/sort.js';
+import { createfilmCard } from './views/film-card.js';
 
 const BEFOREEND = 'beforeend';
 
@@ -7,5 +10,9 @@ const render = (container, template, place) => {
 };
 
 const headerNode = document.body.querySelector('.header');
+const mainNode = document.body.querySelector('.main');
 
 render(headerNode, createProfileTemplate(), BEFOREEND);
+
+render(mainNode, createNavigationTemplate(), BEFOREEND);
+render(mainNode, createSortTemplate(), BEFOREEND);
