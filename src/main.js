57aee24b177af6f9f29b74c279704d7bdec43b04
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-28 07:02:24 +0700

    Рефакторит модуль основного контента

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createProfileTemplate } from './views/profile.js';
 import { createNavigationTemplate } from './views/navigation.js';
 import { createSortTemplate } from './views/sort.js';
 import { createFilmCardTemplate } from './views/film-card.js';
-import { createFilmsListTemplate } from './views/films.js';
-import { createFilmsListExtraTemplate } from './views/films-extra.js';
+import { createFilmsTemplate } from './views/films.js';
 import { createStatisticsTemplate } from './views/statistics.js';
+
+const MAIN_FILMS_AMOUNT = 5;
+const EXTRA_FILMS_AMOUNT = 2;
 
 const AFTERBEGIN = 'afterbegin';
 const BEFOREEND = 'beforeend';
@@ -15,17 +17,23 @@ const render = (container, template, place) => {
 
 const headerNode = document.body.querySelector('.header');
 const mainNode = document.body.querySelector('.main');
-const filmsNode = document.body.querySelector('.films');
 const footerNode = document.body.querySelector('.footer');
 
 render(headerNode, createProfileTemplate(), BEFOREEND);
 
-render(mainNode, createNavigationTemplate(), AFTERBEGIN);
-render(mainNode, createSortTemplate(), AFTERBEGIN);
+render(mainNode, createNavigationTemplate(), BEFOREEND);
+render(mainNode, createSortTemplate(), BEFOREEND);
+render(mainNode, createFilmsTemplate(), BEFOREEND);
 
-render(filmsNode, createFilmsListTemplate(), BEFOREEND);
-render(filmsNode, createFilmsListExtraTemplate('Top rated'), BEFOREEND);
-render(filmsNode, createFilmsListExtraTemplate('Most commented'), BEFOREEND);
+const [
+  mainFilmsListNode,
+  topRatedFilmsListNode,
+  mostCommentedFilmsListNode,
+] = mainNode.querySelectorAll('.films-list__container');
+
+console.log(mainFilmsListNode);
+console.log(topRatedFilmsListNode);
+console.log(mostCommentedFilmsListNode);
 
 render(footerNode, createStatisticsTemplate(), BEFOREEND);
 
