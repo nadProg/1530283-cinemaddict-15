74a4f831dc6374225fef6d3ebc3682c52a456d77
Eugene Skrobov <skrobov.ev@gmail.com>
2021-08-20 19:10:30 +0700

    Создает SmartView

---
+++ src/views/smart.js
@@ -0,0 +1,37 @@
+import AbstractView from './abstract.js';
+import { replace } from '../utils/render.js';
+
+export default class SmartView extends AbstractView {
+  constructor() {
+    super();
+
+    this._data = {};
+  }
+
+  updateData(update, justDataUpdating) {
+    if (!update) {
+      return;
+    }
+
+    this._data = {
+      ...this._data,
+      update,
+    };
+
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    replace(this.getElement(), prevElement);
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {
+    throw new Error('Abstract method not implemented: resetHandlers');
+  }
+}
