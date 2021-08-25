f0b8a8e0a993d7a4c3e393583dd80016e8c2fb6b
Eugene Skrobov <skrobov.ev@gmail.com>
2021-08-25 21:50:49 +0700

    Добавляет экранирование пользовательского ввода

--- src/views/comment.js
+++ src/views/comment.js
@@ -1,4 +1,5 @@
 
+import he from 'he';
 import AbstractView from './abstract.js';
 import { getCommentDate } from '../utils/date.js';
 
@@ -11,7 +12,7 @@ const createCommentTemplate = (comment) => {
         <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
       </span>
       <div>
-        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-text">${he.encode(text)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${getCommentDate(date)}</span>
