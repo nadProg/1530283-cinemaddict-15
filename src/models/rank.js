2ba0b8c594210769d42ead35b98b1366b1849e77
Eugene Skrobov <skrobov.ev@gmail.com>
2021-09-05 20:18:56 +0700

    Рефакторит модели

--- src/models/rank.js
+++ src/models/rank.js
@@ -5,15 +5,15 @@ import AbstractObserver from '../utils/abstract-observer.js';
 export default class RankModel extends AbstractObserver {
   constructor(rank = Rank.NONE) {
     super();
-    this._currentRank = rank;
+    this._rank = rank;
   }
 
   setRank(updateType, rank) {
-    this._currentRank = rank;
+    this._rank = rank;
     this._notify(updateType, rank);
   }
 
   getRank() {
-    return this._currentRank;
+    return this._rank;
   }
 }
